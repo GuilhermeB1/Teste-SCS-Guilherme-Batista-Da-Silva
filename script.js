6ad4e5d3e39ce9e35ae73a1a4c8a95d949a91250
guilhermim30 <batistaguilherme048@gmail.com>
2024-04-13 16:13:48 -0300

    Estilizaco

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const cardsGlobal = document.querySelector(".modelos");
 const cardModelo = document.querySelector(".car-modelo");
-const imagens = ["/src/img/carro-1.svg", "/src/img/carro-2.svg"];
+const imagens = ["src/img/carro-1.svg", "src/img/carro-2.svg"];
 
 for (let i = 0; i < 2; i++) {
   const novoCard = cardModelo.cloneNode(true);
